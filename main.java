e474ea84b09e64b4f79d6608d11e7a8efa597e76
Royalibk <125534696+Royalibk@users.noreply.github.com>
2024-04-03 19:19:57 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,26 @@
+public class Main {
+public static void main(String[] args) {
+  String season= "summer";
+
+  if (season == "winter") {
+    System.out.println("Wear a warm jacket!");
+  }
+  else if (season == "spring") {
+    System.out.println("Wear a T-shirt!");
+  }
+  else if (season == "summer") {
+    System.out.println("Wear a swimming suite!");
+  }
+  else if (season == "autumn") {
+    System.out.println("Wear a rain coat!");
+  }
+
+  else {
+    System.out.println("Wrong season!");
+  }
+
+
+    }
+  }
+
+// @Test
